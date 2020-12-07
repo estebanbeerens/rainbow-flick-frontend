e8b119af75de79c8ac184a365cc86a97e154688b
Esteban Beerens <esteban.beerens@gmail.com>
2020-12-07 20:28:57 +0100

    Added tailwind and basic navigation component

--- src/app/core/models/routes/route-info.ts
+++ src/app/core/models/routes/route-info.ts
@@ -2,14 +2,10 @@ export interface RouteInfo {
     path: string;
     title: string;
     icon: string;
-    class: string;
-    category: string;
 }
 
 export const RouteInfoInitialValue: RouteInfo = {
     path: '',
     title: '',
     icon: '',
-    class: '',
-    category: ''
 };
