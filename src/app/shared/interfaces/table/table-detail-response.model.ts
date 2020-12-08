980d9cd233a0cba986623280433528ea0d0f32c0
Youri Van Laer <info@yourivanlaer.be>
2020-12-08 14:14:33 +0100

    table interface

---
+++ src/app/shared/interfaces/table/table-detail-response.model.ts
@@ -0,0 +1,11 @@
+export interface ITableDetailResponse {
+    id: String,
+    name: String,
+    location: String,
+    imageUrl: String,
+    contactName: String,
+    contactPhone: String,
+    description: String,
+    inUse: Boolean
+}
+
---
+++ src/app/shared/interfaces/table/table-getAll-response.model.ts
@@ -0,0 +1,6 @@
+import { ITableDetailResponse } from 'src/app/shared/interfaces/table/table-detail-response.model';
+
+export interface ITableGetAllResponse {
+    results : ITableDetailResponse[],
+}
+
