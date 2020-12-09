4f746b15f0dfe0bb3dcfd3f02de54d1674095b1e
Esteban Beerens <esteban.beerens@gmail.com>
2020-12-09 18:09:46 +0100

    Minor styling fixes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,13 +8,13 @@ import { SharedRoutingModule } from 'src/app/shared/shared-routing.module';
 import { SharedHomeShellComponent } from './components/shared-home/shared-home-shell/shared-home-shell.component';
 import { SharedHomePresenterComponent } from './components/shared-home/shared-home-presenter/shared-home-presenter.component';
 import { SharedToolbarComponent } from './components/shared-toolbar/shared-toolbar.component';
+import { SharedSearchbarComponent } from './components/shared-searchbar/shared-searchbar.component';
+import { SharedPaginatorComponent } from './components/shared-paginator/shared-paginator.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatRippleModule } from '@angular/material/core';
-import { SharedSearchbarComponent } from './components/shared-searchbar/shared-searchbar.component';
-import { SharedPaginatorComponent } from './components/shared-paginator/shared-paginator.component';
 
 @NgModule({
   declarations: [
