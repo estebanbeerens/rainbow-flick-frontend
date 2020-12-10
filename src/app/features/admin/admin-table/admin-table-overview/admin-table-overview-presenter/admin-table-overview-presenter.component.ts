7447b79feb0e36dca6d99531210639d6a7a7c54b
Youri Van Laer <info@yourivanlaer.be>
2020-12-10 21:41:05 +0100

    pagination

--- src/app/features/admin/admin-table/admin-table-overview/admin-table-overview-presenter/admin-table-overview-presenter.component.ts
+++ src/app/features/admin/admin-table/admin-table-overview/admin-table-overview-presenter/admin-table-overview-presenter.component.ts
@@ -7,11 +7,34 @@ import { ITableDetails } from 'src/app/shared/interfaces/table/table-details.mod
   styleUrls: ['./admin-table-overview-presenter.component.scss'],
 })
 export class AdminTableOverviewPresenterComponent implements OnInit {
+  //table pagination
+  minTable: number = 0;
+  splitEvery: number = 5;
+  total: number;
+
+  viewTable: ITableDetails[];
   @Input() tables: ITableDetails[];
   @Output() delete = new EventEmitter();
   @Output() actionButton = new EventEmitter();
 
   //TODO PRELOADER
+
+  ngOnChanges() {
+    this.total = this.tables.length;
+    this.viewTable = this.tables.slice(this.minTable, this.splitEvery);
+  }
+
+  tablePagination(min: number, max: number) {
+    this.viewTable = this.tables.slice(min, max);
+  }
+
+  clickPrevious(result) {
+    this.tablePagination(result.from, result.to);
+  }
+
+  clickNext(result) {
+    this.tablePagination(result.from, result.to);
+  }
 
   clickView(tableID: String): void {
     console.log('click view');
@@ -21,7 +44,10 @@ export class AdminTableOverviewPresenterComponent implements OnInit {
   clickDelete(tableID: String): void {
     this.delete.emit(tableID);
   }
-  constructor() {}
+  // constructor() {
+  //   this.viewTable = this.tables.slice(this.minTable, this.splitEvery - 1);
+  //   console.log(this.viewTable);
+  // }
 
   ngOnInit(): void {}
 }
