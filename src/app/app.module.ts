dffff1505f22b5e0ee53ab8cf84d44039e2bc3ff
Esteban Beerens <esteban.beerens@gmail.com>
2020-12-10 14:25:00 +0100

    Created and implemented shared-date-changer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeNlBe from '@angular/common/locales/nl-BE';
 
+registerLocaleData(localeNlBe);
 
 @NgModule({
   declarations: [
