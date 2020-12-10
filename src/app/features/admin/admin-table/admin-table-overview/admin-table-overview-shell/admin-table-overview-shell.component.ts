c1f89a415b8e6ee16b499c9fe19bf1935576d933
Tom Bosmans <r0621531@student.thomasmore.be>
2020-12-10 14:04:49 +0100

    Update admin-table-overview-shell.component.ts

--- src/app/features/admin/admin-table/admin-table-overview/admin-table-overview-shell/admin-table-overview-shell.component.ts
+++ src/app/features/admin/admin-table/admin-table-overview/admin-table-overview-shell/admin-table-overview-shell.component.ts
@@ -1,39 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { table } from 'console';
-import { UserTableDetailsShellComponent } from 'src/app/features/user/user-table/user-table-details/user-table-details-shell/user-table-details-shell.component';
 import { TableService } from 'src/app/services/table.service';
 import { ITableDetails } from 'src/app/shared/interfaces/table/table-details.model';
 
 @Component({
-  selector: 'app-admin-table-overview-shell',
-  templateUrl: './admin-table-overview-shell.component.html',
-  styleUrls: ['./admin-table-overview-shell.component.scss'],
+  selector: 'app-user-table-overview-shell',
+  templateUrl: './user-table-overview-shell.component.html',
+  styleUrls: ['./user-table-overview-shell.component.scss'],
 })
-export class AdminTableOverviewShellComponent implements OnInit {
+export class UserTableOverviewShellComponent implements OnInit {
   tables$: ITableDetails[];
 
-  constructor(private _tableService: TableService, public dialog: MatDialog) {}
-
+  constructor(private _tableService: TableService) {}
   ngOnInit(): void {
     this._tableService.loadTables();
     this._tableService.tables$.subscribe((table) => (this.tables$ = table));
-  }
-
-  createTable(tableID: String) {
-    //TODO fix create table
-    // this._tableService.createTag()
-  }
-
-  deleteTable(tableID: String) {
-    this._tableService.deleteTable(tableID);
-  }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(UserTableDetailsShellComponent, {
-      width: '250px',
-    });
-
-    dialogRef.componentInstance.ngOnInit();
   }
 }
