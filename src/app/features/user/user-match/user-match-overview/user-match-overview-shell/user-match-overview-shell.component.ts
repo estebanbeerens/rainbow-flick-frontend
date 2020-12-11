db2075d301b6dc1259da7ae25e735fd6d0945fb6
Esteban Beerens <esteban.beerens@gmail.com>
2020-12-11 15:39:18 +0100

    Minor styling fix & navigate to table on match add

--- src/app/features/user/user-match/user-match-overview/user-match-overview-shell/user-match-overview-shell.component.ts
+++ src/app/features/user/user-match/user-match-overview/user-match-overview-shell/user-match-overview-shell.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-match-overview-shell',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserMatchOverviewShellComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToTables(): void {
+    this.router.navigate(['/app/user/table']);
+    this.snackBar.open('Kies hier een datum & tafel om uw match aan te maken.', '', {
+      duration: 5000
+    });
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -16,6 +16,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatRippleModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AuthUserInTeamPipe } from 'src/app/shared/pipes/auth-user-in-team.pipe';
 import { TruncatePipe } from 'src/app/shared/pipes/truncate.pipe';
 import { AuthUserTeamCaptainPipe } from 'src/app/shared/pipes/auth-user-team-captain.pipe';
@@ -53,6 +54,7 @@ import { AuthUserInRequestedParticipantsPipe } from 'src/app/shared/pipes/auth-u
     MatTooltipModule,
     MatDialogModule,
     MatRippleModule,
+    MatSnackBarModule,
     FormsModule,
     ReactiveFormsModule,
     NgScrollbarModule,
