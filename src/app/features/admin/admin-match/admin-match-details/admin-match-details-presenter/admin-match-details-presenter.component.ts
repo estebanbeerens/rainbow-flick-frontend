1d3a9f58d0628c1e773200eca91d453b0bead50d
Youri Van Laer <info@yourivanlaer.be>
2020-12-11 14:50:02 +0100

    Admin display Matches

--- src/app/features/admin/admin-match/admin-match-details/admin-match-details-presenter/admin-match-details-presenter.component.ts
+++ src/app/features/admin/admin-match/admin-match-details/admin-match-details-presenter/admin-match-details-presenter.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IMatchDetail } from 'src/app/shared/interfaces/match/match-details.model';
 
 @Component({
   selector: 'app-admin-match-details-presenter',
   templateUrl: './admin-match-details-presenter.component.html',
-  styleUrls: ['./admin-match-details-presenter.component.scss']
+  styleUrls: ['./admin-match-details-presenter.component.scss'],
 })
 export class AdminMatchDetailsPresenterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() match: IMatchDetail;
+  @Input() formGroup;
+  @Output() closeDialog = new EventEmitter();
+  @Output() submitForm = new EventEmitter();
+  constructor() {}
+  ngOnInit(): void {}
+  clickSubmitForm(): void {
+    this.submitForm.emit();
   }
-
+  addCloseDialog(): void {
+    this.closeDialog.emit();
+  }
 }
