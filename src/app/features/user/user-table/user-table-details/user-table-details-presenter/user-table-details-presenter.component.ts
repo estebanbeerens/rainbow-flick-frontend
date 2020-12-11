df58d69f3c45bcd88ec4bb87175c0bcb3e474a72
Youri Van Laer <info@yourivanlaer.be>
2020-12-11 19:59:18 +0100

    User tables details

--- src/app/features/user/user-table/user-table-details/user-table-details-presenter/user-table-details-presenter.component.ts
+++ src/app/features/user/user-table/user-table-details/user-table-details-presenter/user-table-details-presenter.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ITableOverviewDetails } from 'src/app/shared/interfaces/table/tables-overview-details.model';
 
 @Component({
   selector: 'app-user-table-details-presenter',
   templateUrl: './user-table-details-presenter.component.html',
-  styleUrls: ['./user-table-details-presenter.component.scss']
+  styleUrls: ['./user-table-details-presenter.component.scss'],
 })
 export class UserTableDetailsPresenterComponent implements OnInit {
+  @Input() table: ITableOverviewDetails;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/features/user/user-table/user-table-details/user-table-details-shell/user-table-details-shell.component.ts
+++ src/app/features/user/user-table/user-table-details/user-table-details-shell/user-table-details-shell.component.ts
@@ -1,13 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { TableService } from 'src/app/services/table.service';
+import { ActivatedRoute } from '@angular/router';
+import { SearchFilterPipe } from 'src/app/shared/pipes/search-filter.pipe';
+import { Observable } from 'rxjs';
+import { ITableOverviewDetails } from 'src/app/shared/interfaces/table/tables-overview-details.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-table-details-shell',
   templateUrl: './user-table-details-shell.component.html',
   styleUrls: ['./user-table-details-shell.component.scss'],
+  providers: [SearchFilterPipe],
 })
 export class UserTableDetailsShellComponent implements OnInit {
-  constructor(private _tableService: TableService) {}
+  getId: String;
+  table$: Observable<ITableOverviewDetails>;
+  constructor(
+    private _tableService: TableService,
+    private _route: ActivatedRoute,
+    private _searchFilterPipe: SearchFilterPipe
+  ) {}
+  ngOnInit(): void {
+    this.getId = this._route.snapshot.paramMap.get('id');
+    this._tableService.tableOverview$.subscribe((result) => console.log(result));
 
-  ngOnInit(): void {}
+    // get element
+    this.table$ = this._tableService.tableOverview$.pipe(
+      map((items) => {
+        return items.tables.find((item) => item.id == this.getId);
+      })
+    );
+  }
 }
