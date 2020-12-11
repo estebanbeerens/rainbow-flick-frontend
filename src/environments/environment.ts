457bd92a5fd6cb01d250d613dc61e6d399903573
Youri Van Laer <info@yourivanlaer.be>
2020-12-11 10:47:11 +0100

    Local api change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   // TODO : Edit apiUrl to something that is right
-  apiUrl: 'https://rainbow-flick-backend-app.herokuapp.com/api/',
+  // apiUrl: 'https://rainbow-flick-backend-app.herokuapp.com/api/',
+  apiUrl: 'http://localhost:8080/api/',
 };
 
 /*
