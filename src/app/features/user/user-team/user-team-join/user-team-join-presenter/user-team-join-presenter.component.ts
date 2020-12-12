17c435692ea6de9398dceee5ad724e438016acb1
Esteban Beerens <esteban.beerens@gmail.com>
2020-12-12 21:53:45 +0100

    Added link from join to details

--- src/app/features/user/user-team/user-team-join/user-team-join-presenter/user-team-join-presenter.component.ts
+++ src/app/features/user/user-team/user-team-join/user-team-join-presenter/user-team-join-presenter.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { TeamService } from 'src/app/services/team.service';
 import { ITeamOverview } from 'src/app/shared/interfaces/team/team-overview.model';
 
 @Component({
@@ -11,6 +13,16 @@ export class UserTeamJoinPresenterComponent {
 
   @Input() teams: ITeamOverview[];
   @Output() onJoinTeam = new EventEmitter<String>();
+
+  constructor(
+    private _teamService: TeamService,
+    private _router: Router
+  ) {}
+
+  rowClicked(teamID: string){
+    this._teamService.loadTeamDetails(teamID);
+    this._router.navigate([`app/user/team/details/${teamID}`]);
+  }
   
   joinTeam(teamID:String){
     this.onJoinTeam.emit(teamID);
