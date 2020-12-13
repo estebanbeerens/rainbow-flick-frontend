05fd1764907ca2ff6f56d50cc290eb0bf5964965
Tom Bosmans <r0621531@student.thomasmore.be>
2020-12-13 15:04:55 +0100

    added guards

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CoreContentComponent } from 'src/app/core/components/content/content.component';
+import { AdminCreateGuard } from 'src/app/security/guards/admin-create.guard';
 
 const routes: Routes = [
 	{
@@ -9,7 +10,7 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'user', pathMatch: 'full' },
       { path: 'user', loadChildren: () => import('../features/user/user.module').then(m => m.UserModule) },
-      { path: 'admin', loadChildren: () => import('../features/admin/admin.module').then(m => m.AdminModule) },
+      { path: 'admin', loadChildren: () => import('../features/admin/admin.module').then(m => m.AdminModule),canActivate: [AdminCreateGuard] },
       { path: '**', redirectTo: 'home' }
     ]
 	},
--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -9,59 +9,76 @@ import { AdminTeamDetailsShellComponent } from 'src/app/features/admin/admin-tea
 import { AdminTeamOverviewShellComponent } from 'src/app/features/admin/admin-team/admin-team-overview/admin-team-overview-shell/admin-team-overview-shell.component';
 import { AdminUserDetailsShellComponent } from 'src/app/features/admin/admin-user/admin-user-details/admin-user-details-shell/admin-user-details-shell.component';
 import { AdminUserOverviewShellComponent } from 'src/app/features/admin/admin-user/admin-user-overview/admin-user-overview-shell/admin-user-overview-shell.component';
+import { AdminCreateGuard } from 'src/app/security/guards/admin-create.guard';
+import { MatchReadGuard } from 'src/app/security/guards/match-read.guard';
+import { TableReadGuard } from 'src/app/security/guards/table-read.guard';
+import { TeamReadGuard } from 'src/app/security/guards/team-read.guard';
+import { UserReadGuard } from 'src/app/security/guards/user-read.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    component: AdminHomeComponent
+    component: AdminHomeComponent,
   },
   {
     path: 'table/overview',
     component: AdminTableOverviewShellComponent,
+    canActivate: [TableReadGuard]
   },
   {
     path: 'table/details/:id',
     component: AdminTableDetailsShellComponent,
+    canActivate: [TableReadGuard]
   },
   {
     path: 'table',
     redirectTo: 'table/overview',
+    canActivate: [TableReadGuard]
   },
   {
     path: 'team/overview',
     component: AdminTeamOverviewShellComponent,
+    canActivate: [TeamReadGuard]
   },
   {
     path: 'team/details/:id',
     component: AdminTeamDetailsShellComponent,
+    canActivate: [TeamReadGuard]
   },
   {
     path: 'team',
     redirectTo: 'team/overview',
+    canActivate: [TeamReadGuard]
   },
   {
     path: 'user/overview',
     component: AdminUserOverviewShellComponent,
+    canActivate: [UserReadGuard]
   },
   {
     path: 'user/details/:id',
     component: AdminUserDetailsShellComponent,
+    canActivate: [UserReadGuard]
   },
   {
     path: 'user',
     redirectTo: 'user/overview',
+    canActivate: [UserReadGuard]
   },
   {
     path: 'match/overview',
     component: AdminMatchOverviewShellComponent,
+    canActivate: [MatchReadGuard]
   },
   {
     path: 'match/details/:id',
     component: AdminMatchDetailsShellComponent,
+    canActivate: [MatchReadGuard]
   },
   {
     path: 'match',
     redirectTo: 'match/overview',
+    canActivate: [MatchReadGuard]
   },
   {
     path: '**',
